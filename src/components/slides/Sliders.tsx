37cb5590306e76f47c51bb20fa0b2de79c5c78b2
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2024-02-19 15:16:49 -0300

    feat: create component slider and configure the lib swiper for apresentation the images on slide

---
+++ src/components/slides/Sliders.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Image, { StaticImageData } from 'next/image';
+
+type PhotoProtocol = {
+  _id: number;
+  src: StaticImageData;
+  alt: string;
+  title: string;
+  description: string;
+};
+
+type SlidersProtocol = {
+  galleryPhotos: PhotoProtocol[];
+  setSlide: React.Dispatch<React.SetStateAction<number>>;
+};
+
+function Sliders({ galleryPhotos, setSlide }: SlidersProtocol) {
+  return (
+    <div className="w-1/2">
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+        spaceBetween={20}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        onSlideChange={(value) => setSlide(value.activeIndex)}
+        className="rounded-[20px] shadowCustom"
+        autoplay={{ delay: 3000 }}
+      >
+        {galleryPhotos.map((photo) => (
+          <SwiperSlide key={photo._id} className="">
+            <Image src={photo.src} alt={photo.alt} className="h-[540px] w-full rounded-[20px]" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
+
+export default Sliders;
