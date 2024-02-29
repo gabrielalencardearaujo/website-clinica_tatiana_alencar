f83aaad84662409096bcc18a52d7f04c5107b99e
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2024-02-29 10:01:25 -0300

    fix: changes layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`bg-white ${poppins.className}`}>
         <Header />
-        {children}
+        <main className="w-11/12 mx-auto flex flex-col gap-32">{children}</main>
         <Footer />
       </body>
     </html>
