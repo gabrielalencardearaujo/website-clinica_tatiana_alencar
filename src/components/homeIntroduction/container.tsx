b7b92c0f487b76bcb89b95e3b76b6c96c9f24f7f
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2024-03-14 13:32:37 -0300

    feat: add responsive layout for introduction in home

--- src/components/homeIntroduction/container.tsx
+++ src/components/homeIntroduction/container.tsx
@@ -22,43 +22,51 @@ const playfairDisplaySC = Playfair_Display_SC({
 
 function IntroductionHome() {
   return (
-    <section className="bg-gradient-to-t from-color-base-1 min-h-[800px] rounded-[20px] flex px-20 mb-26 mt-6">
-      <div className="flex flex-col justify-center self-center gap h-5/6 w-7/12 gap-y-32">
-        <article className="flex flex-col gap-y-9 text-color-base-8 animeEntryLeftSlow">
+    <section className="bg-gradient-to-t from-color-base-1 3xl:min-h-[800px] xxxl:min-h-[720px] xxl:min-h-[620px] xlg:min-h-[500px] xl:min-h-[420px] md:h-[400px] xm:min-h-[520px] rounded-[20px] flex xxl:px-20 xl:px-10 mb-26 mt-6">
+      <div className="flex flex-col xlg:justify-center xl:justify-center self-center h-5/6 xl:w-7/12 xxl:gap-y-32 md:gap-y-20 gap-y-10 w-full">
+        <article className="flex flex-col gap-y-9 text-color-base-8 animeEntryLeftSlow md:px-20 sm:px-10 xm:px-4 xl:px-0">
           <div>
-            <p className={`${playfairDisplaySC.className} text-3xl -mb-8`}>Clínica</p>
-            <h1 className={`text-[144px] font-display ${pinyonScript.className}`}>
+            <p className={`${playfairDisplaySC.className} xl:text-xl sm:text-lg text-md -mb-8`}>
+              Clínica
+            </p>
+            <h1
+              className={`3xl:text-[144px] xxxl:text-9xl xxl:text-8xl xl:text-7xl sm:text-6xl text-5xl mt-10 font-display ${pinyonScript.className}`}
+            >
               Tatiana Alencar
             </h1>
           </div>
-          <p className="w-9/12 text-color-base-7">
+          <p className="xl:w-9/12 text-color-base-7 xxxl:text-lg xxl:text-md text-sm">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed hic inventore placeat.
             Autem rerum reiciendis quia eius voluptas nihil alias nulla officia unde similique.
           </p>
         </article>
 
-        <article className="flex gap-x-10 animeEntry">
+        <article className="flex flex-col md:flex-row items-start md:gap-x-10 md:gap-y-0 sm:gap-y-5 xm:gap-y-3 animeEntry md:px-20 sm:px-10 xm:px-4 xl:px-0 xl:justify-start justify-between">
           <Link
             href={'/appointments'}
-            className="flex content-center gap-2 justify-center bg-color-base-8 py-4 px-6 text-color-base-0 rounded-[20px] text-xl font-semibold"
+            className="flex content-center xxl:gap-2 xl:gap-1 justify-center bg-color-base-8 xxxl:py-4 xxxl:px-6 sm:px-3 sm:py-2 xm:px-2 xm:py-1 text-color-base-0 rounded-[20px] xxxl:text-xl xl:text-md text-sm font-semibold sm:min-h-[60px] xxxl:h-full xm:w-full md:w-auto"
           >
             <p className="self-center">Marque sua Consulta</p>
-            <Image src={ArrowRight} alt="arrow right" />
+            <Image className="xxl:w-[67px] xl:w-[40px]" src={ArrowRight} alt="arrow right" />
           </Link>
 
           <Link
             href={'https://api.whatsapp.com/send/?phone=' + env.PHONE_TATIANA_ALENCAR}
             target="_blank"
-            className="flex content-center gap-4 justify-center border-2 border-color-base-7 py-4 px-6 text-color-base-7 rounded-[20px] text-xl font-semibold"
+            className="flex content-center gap-4 justify-center border-2 border-color-base-7 xxxl:py-4 xxxl:px-6 sm:px-3 sm:py-2 xm:px-2 xm:py-1 text-color-base-7 rounded-[20px] xxxl:text-xl xl:text-md text-sm font-semibold sm:min-h-[60px] xxxl:h-full xm:w-full md:w-auto"
           >
             <p className="self-center">Fale Conosco</p>
-            <Image src={WhatsappIcon} alt="whatsapp icon" width={32} />
+            <Image className="" src={WhatsappIcon} alt="whatsapp icon" width={32} />
           </Link>
         </article>
       </div>
 
-      <div className="animeEntryRightSlow">
-        <Image src={ImageWomenIntro} alt="image women model" className="h-full" />
+      <div className="animeEntryRightSlow xl:block hidden">
+        <Image
+          src={ImageWomenIntro}
+          alt="image women model"
+          className="h-full xlg:w-full xl:w-full xl:h-10/12 "
+        />
       </div>
     </section>
   );
