5b3cc466970170535efddbe8da95b526d0039332
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2024-03-27 18:24:14 -0300

    feat: create page login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,13 @@
+import LoginForm from '@/components/forms/LoginForm';
 import React from 'react';
 
 export default function Login() {
   return (
-    <section>
-      <p>Rota de Login!</p>
+    <section className="flex">
+      <div className="w-1/4 m-auto min-w-[480px] shadowCustom rounded-[20px] py-6 px-4">
+        <h3 className="sm:text-2xl text-lg text-color-base-8 mx-2 mb-4">Faça seu login·</h3>
+        <LoginForm />
+      </div>
     </section>
   );
 }
---
+++ src/components/forms/LoginForm/index.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import Button from '@/components/buttons/Button';
+import React from 'react';
+import InputForm from '../InputForm';
+import { useFormState, useFormStatus } from 'react-dom';
+import { handleLogin } from '@/actions/login';
+
+const ButtonLogin = () => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      className="mt-10 flex self-end content-center gap-2 bg-color-base-8 justify-center xl:py-4 xl:px-6 py-3 px-4 text-color-base-0 rounded-[20px] md:text-2xl text-lg font-semibold tracking-wider ms-2 w-full"
+      type="submit"
+      disabled={pending}
+    >
+      <p className="flex self-center content-center">Entrar</p>
+    </Button>
+  );
+};
+
+function LoginForm() {
+  const [state, action] = useFormState(handleLogin, {
+    authorization: false,
+    data: {
+      email: '',
+      name: '',
+      role: 0,
+    },
+    error: [],
+  });
+
+  return (
+    <div>
+      <form action={action} className="flex flex-col gap-y-4">
+        <InputForm title="Email" type="email" name="email" id="email" required autoComplete="on" />
+        <InputForm
+          title="Senha"
+          type="password"
+          name="password"
+          id="password"
+          required
+          autoComplete="on"
+        />
+        {!state.authorization &&
+          state.error.map((e, i) => (
+            <span key={i} className="text-color-error ms-2">
+              {e}
+            </span>
+          ))}
+        <ButtonLogin />
+      </form>
+    </div>
+  );
+}
+
+export default LoginForm;
